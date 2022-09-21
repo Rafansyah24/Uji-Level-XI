c7969d02cb0a943c32e99c0c2b2d41bb27384861
Rafansyah24 <rafansyah.209@gmail.com>
2022-09-21 09:42:34 +0700

    melakukan perubahan page data siswa

--- data-siswa.php
+++ data-siswa.php
@@ -8,6 +8,7 @@
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <script src="https://kit.fontawesome.com/fcf02eb2a6.js" crossorigin="anonymous"></script>
         <link rel="stylesheet" href="style.css">
         <title>Pustaka Online</title>
     </head>
@@ -17,9 +18,11 @@
             <h3><span class="logo">PUSTAKA</span></h3>
             <div class="menu">
                 <div class="button">
+                <i class="fa-solid fa-user"></i>
                     <p><a href="data-siswa.php">Data Siswa</a></p>
                 </div>
                 <div class="button">
+                <i class="fa-sharp fa-solid fa-book"></i>
                     <p><a href="data-buku.php">Data Buku</a></p>
                 </div>
             </div>
@@ -33,14 +36,26 @@
             <div class="link_tmbh">
                 <h4> <a href="tambah-siswa.php">[+] Tambah Data</a> </h4>
             </div>
+
+            <ul>
+                <li><a href="#"><i class="fa-solid fa-chevron-left"></i></a></li>
+                <li><a href="#" class="active">1</a></li>
+                <li><a href="#">2</a></li>
+                <li><a href="#">3</a></li>
+                <li><a href="#">4</a></li>
+                <li><a href="#"><i class="fa-solid fa-chevron-right"></i></a></li>
+            </ul>
+
             <div class="table">
                 <table cellspacing="0">
                         <tr>
-                            <th>NIS</th>
-                            <th>Nama</th>
-                            <th>No.Telepon</th>
-                            <th>Alamat</th>
-                            <th>Action</th>
+                            <div class="table-row">
+                                <th style="border-top-left-radius: 10px;">NIS</th>
+                                <th>Nama</th>
+                                <th>No.Telepon</th>
+                                <th>Alamat</th>
+                                <th style="border-top-right-radius: 10px;">Action</th>
+                            </div>
                         </tr>
 
 
@@ -55,8 +70,8 @@
                                     <td style="text-align: center;"><?=$s['no_telepon']?></td>
                                     <td style="text-align: center;"><?=$s['alamat']?></td>
 
-                                    <td style="text-align: center;"><a href="form-edit.php?NIS=<?=$s['NIS']?>" id=a1>Edit</a> 
-                                                                    <a href="hapus.php?NIS=<?=$s['NIS']?>" id= a2>Hapus</a></td>
+                                    <td style="text-align: center;"><a href="form-edit.php?NIS=<?=$s['NIS']?>" id=a1><i class="fa-solid fa-pen-to-square"></i></a> 
+                                                                    <a href="hapus.php?NIS=<?=$s['NIS']?>" id= a2><i class="fa-solid fa-trash"></i></a></td>
                                 </tr>
                             
                         <?php
--- tambah-siswa.php
+++ tambah-siswa.php
@@ -4,18 +4,21 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://kit.fontawesome.com/fcf02eb2a6.js" crossorigin="anonymous"></script>
     <title>Pustaka Online</title>
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
 <div class="container">
         <div class="sidebar">
-            <h3><span class="logo"><a href="index.php" style ="text-decoration: none ; color: #312c38;">PUSTAKA</a></span></h3>
+            <h3><span class="logo">PUSTAKA</span></h3>
             <div class="menu">
                 <div class="button">
+                <i class="fa-solid fa-user"></i>
                     <p><a href="data-siswa.php">Data Siswa</a></p>
                 </div>
                 <div class="button">
+                <i class="fa-sharp fa-solid fa-book"></i>
                     <p><a href="data-buku.php">Data Buku</a></p>
                 </div>
             </div>
